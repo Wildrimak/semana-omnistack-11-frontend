741ff3eb3594a9a719ba4f655ecf7ef1a17c0a9e
Wildrimak <finalwildrimak@gmail.com>
2020-03-26 01:33:27 -0300

    4. Pagina de Register e Profile concluidas

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 import './global.css';
 
-import Logon from './pages/Logon'
+import Routes from './routes';
 
 function App() {
     
   return (
     <div>
-      <Logon> 
-      </Logon>
+      <Routes />
     </div>
   );
 }
